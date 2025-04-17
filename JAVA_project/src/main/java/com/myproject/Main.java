7f4be83afa2629403e6c09db2113697581b02552
BUPTxiaoxu <624059083@qq.com>
2025-04-17 21:39:06 +0800

    主框架

---
+++ JAVA_project/src/main/java/com/myproject/Main.java
@@ -0,0 +1,16 @@
+package main.java.com.myproject;
+
+import main.java.com.myproject.gui.MainFrame;
+import javax.swing.SwingUtilities;
+
+public class Main {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                MainFrame mainFrame = new MainFrame();
+                mainFrame.setVisible(true);
+            }
+        });
+    }
+}
---
+++ JAVA_project/src/main/java/com/myproject/util/CsvUtil.java
@@ -0,0 +1,4 @@
+package main.java.com.myproject.util;
+
+public class CsvUtil {
+}
